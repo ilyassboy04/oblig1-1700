a4d0d93ed96c6302a834a2713e30ef414251642f
ilyassboy04 <elsah1344@oslomet.no>
2024-02-17 09:46:16 +0100

    Opprettelse av springboot project og oppdatering av readmefil

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # oblig1-1700
+Oslomet brukernavn: elsah1344
+
+Github brukernavn: ilyassboy04
+
+Github repo URL: https://github.com/ilyassboy04/oblig1-1700.git
+
+Fullt navn: Elias Sahraoui
+
+Kort beskrivelse av applikasjon 5-10(setninger):
---
+++ src/test/java/com/example/oblig1_1700/Oblig11700ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1_1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig11700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
